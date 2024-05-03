d619dc257d4f6cb1eb4c0763565386c13d69a8d7
sb-nour <nour@simplebackups.com>
2024-05-03 06:32:00 +0300

    refactor: Add function to retrieve regions from multiple providers
    This commit refactors the code in the `api/index.go` file to add a new function `getRegions()` that retrieves regions from multiple providers. The function uses goroutines and channels to concurrently fetch regions from different providers and aggregates the results into a map. This improves the efficiency of retrieving regions and allows for easy addition of new providers in the future.
    
    The `Handler` function is also updated to call the `getRegions()` function and return the regions as JSON in the response.
    
    This commit addresses the need to retrieve regions from multiple providers and improves the overall performance of the application.

--- api/index.go
+++ api/index.go
@@ -3,17 +3,74 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"sync"
 
+	"github.com/sb-nour/providers-endpoints/service"
 	gee "github.com/tbxark/g4vercel"
 )
+
+type ProviderRegions struct {
+	Provider string
+	Regions  service.Regions
+}
+
+func getRegions() map[string]service.Regions {
+	var wg sync.WaitGroup
+	providerRegions := make(chan ProviderRegions)
+
+	providers := []struct {
+		name string
+		fn   func() service.Regions
+	}{
+		{"AWS", service.GetAmazonRegions},
+		{"LIGHTSAIL", service.GetLightsailRegions},
+		{"DIGITALOCEAN", service.GetDigitalOceanRegions},
+		{"UPCLOUD", service.GetUpcloudRegions},
+		{"EXOSCALE", service.GetExoscaleRegions},
+		{"WASABI", service.GetWasabiRegions},
+		{"GOOGLE_CLOUD", service.GetGoogleCloudRegions},
+		{"BACKBLAZE", service.GetBackblazeRegions},
+		{"LINODE", service.GetLinodeRegions},
+		{"OUTSCALE", service.GetOutscaleRegions},
+		{"STORJ", service.GetStorjRegions},
+		{"VULTR", service.GetVultrRegions},
+	}
+
+	for _, provider := range providers {
+		wg.Add(1)
+		go func(provider struct {
+			name string
+			fn   func() service.Regions
+		}) {
+			defer wg.Done()
+			providerRegions <- ProviderRegions{Provider: provider.name, Regions: provider.fn()}
+		}(provider)
+	}
+
+	go func() {
+		wg.Wait()
+		close(providerRegions)
+	}()
+
+	regions := make(map[string]service.Regions)
+	for pr := range providerRegions {
+		regions[pr.Provider] = pr.Regions
+	}
+
+	return regions
+}
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	server := gee.New()
 
 	server.GET("/", func(context *gee.Context) {
-		context.JSON(200, gee.H{
-			"message": "hello go from vercel !!!!",
-		})
+		regions := getRegions()
+		// regionsJson, err := json.Marshal(regions)
+		// if err != nil {
+		// 	fmt.Println("Error marshalling JSON:", err)
+		// 	return
+		// }
+		context.JSON(200, regions)
 	})
 	server.GET("/hello", func(context *gee.Context) {
 		name := context.Query("name")
