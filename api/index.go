7f596a14c329672ee2ea93ce670f3ca3bd57d5be
sb-nour <nour@simplebackups.com>
2024-05-03 06:46:59 +0300

    refactor: Update Handler to retrieve regions from getRegions function
    The code changes in `api/index.go` refactor the `Handler` function to retrieve regions from the newly added `getRegions` function. This improves the code structure and separates the logic for retrieving regions from the logic for handling HTTP requests.

--- api/index.go
+++ api/index.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
 	"sync"
 
@@ -62,36 +61,16 @@ func getRegions() map[string]service.Regions {
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	server := gee.New()
-
+	regions := getRegions()
 	server.GET("/", func(context *gee.Context) {
-		regions := getRegions()
 		context.JSON(200, regions)
 	})
-	server.GET("/hello", func(context *gee.Context) {
-		name := context.Query("name")
-		if name == "" {
-			context.JSON(400, gee.H{
-				"message": "name not found",
-			})
-		} else {
-			context.JSON(200, gee.H{
-				"data": fmt.Sprintf("Hello %s!", name),
-			})
+	server.GET("/:key", func(context *gee.Context) {
+		if _, ok := regions[context.Param("key")]; !ok {
+			context.JSON(404, "Not Found")
+			return
 		}
-	})
-	server.GET("/user/:id", func(context *gee.Context) {
-		context.JSON(400, gee.H{
-			"data": gee.H{
-				"id": context.Param("id"),
-			},
-		})
-	})
-	server.GET("/long/long/long/path/*test", func(context *gee.Context) {
-		context.JSON(200, gee.H{
-			"data": gee.H{
-				"url": context.Path,
-			},
-		})
+		context.JSON(200, regions[context.Param("key")])
 	})
 	server.Handle(w, r)
 }
